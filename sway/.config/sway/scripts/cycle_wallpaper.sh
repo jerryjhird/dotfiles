dc6be774f1f1cd9080543f26fb519b3229b60629
Noface <166717111+Jerryslang@users.noreply.github.com>
2025-07-31 14:44:33 +0100

    Update cycle_wallpaper.sh

--- sway/.config/sway/scripts/cycle_wallpaper.sh
+++ sway/.config/sway/scripts/cycle_wallpaper.sh
@@ -3,10 +3,22 @@
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
 SYMLINK="$HOME/Pictures/sym_wallpaper"
 
-mapfile -t wallpapers < <(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | sort)
+prefix="wp_"
 
-current=$(readlink "$SYMLINK")
+# If '--unsafe' is passed, switch to 'swp_'
+if [[ "$1" == "--unsafe" ]]; then
+    prefix="swp_"
+fi
 
+mapfile -t wallpapers < <(find "$WALLPAPER_DIR" -type f \
+    \( -iname "${prefix}*.jpg" -o -iname "${prefix}*.png" -o -iname "${prefix}*.jpeg" \) | sort)
+
+if [[ ${#wallpapers[@]} -eq 0 ]]; then
+    echo "No wallpapers found with prefix '${prefix}'"
+    exit 1
+fi
+
+current=$(readlink "$SYMLINK") # get the current symlink target and determine current index
 for i in "${!wallpapers[@]}"; do
     if [[ "${wallpapers[$i]}" == "$current" ]]; then
         index=$i
